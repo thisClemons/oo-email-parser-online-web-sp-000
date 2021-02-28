09ba8e027b793e51512ee13cbc4e88a240c606f7
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-28 22:47:09 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,12 +5,14 @@
 
 class EmailAddressParser
 
+  attr_reader :emails
+
   def initialize(emails)
     @emails = emails
   end
 
   def parse
-    @emails.split(/,? /).uniq 
+    emails.split(/,? /).uniq
 
 
   end
